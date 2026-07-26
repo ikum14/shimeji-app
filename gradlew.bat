782f4fa84ff37ee41314eed9287f3b7693c714f1
ilham <163923853+ikum14@users.noreply.github.com>
2026-07-26 15:04:03 +0700

    Fix 5: Regenerate Gradle wrapper files with correct content

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%"==\"\" @echo off
+@if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -25,18 +25,11 @@
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
-if "%DIRNAME%"==\"\" set DIRNAME=.
-set APP_BASE_NAME=%~n0
+if "%DIRNAME%"=="" set DIRNAME=.
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any \".\") links by re-executing the command for those paths
-@rem which may still be quoted.
-set APP_HOME=%~dp0
-
-if exist "%APP_HOME%CLASSPATH" (
-    set /p APP_CLASSPATH=<"%APP_HOME%CLASSPATH"
-    set CLASSPATH=%APP_CLASSPATH%;%CLASSPATH%
-)
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
@@ -46,10 +39,10 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >nul 2>&1
-if "%ERRORLEVEL%"==\"0\" goto execute
+if %ERRORLEVEL% equ 0 goto execute
 
 echo.
-echo Error: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
@@ -63,7 +56,7 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo Error: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
@@ -76,14 +69,11 @@ goto fail
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% \"-Dorg.gradle.appname=%APP_BASE_NAME%\" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_0" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@endlocal & set ERROR_CODE=%ERRORLEVEL%
-
-if not "%ERROR_CODE%"==\"0\" goto fail
-
-exit /b %ERRORCODE%
+@rem End local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" endlocal
 
 :fail
 exit /b 1
